5e2b6fb619551b10b98596e024b12712a23d21b0
Jinsujin <sylpid003@gmail.com>
2023-01-28 18:17:30 +0900

    Feat: 캐러셀 화면 생성, 메인화면 UI (#1)

---
+++ UI-Archive/UI-Archive/Carousel/CarouselViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class CarouselViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .orange
+    }
+
+}
--- UI-Archive/UI-Archive/ViewController.swift
+++ UI-Archive/UI-Archive/ViewController.swift
@@ -1,19 +1,15 @@
-//
-//  ViewController.swift
-//  UI-Archive
-//
-//  Created by Sujin Jin on 2023/01/28.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchedCarouselButton(_ sender: Any) {
+        let viewController = CarouselViewController()
+        present(viewController, animated: false)
+    }
+    
 }
 
