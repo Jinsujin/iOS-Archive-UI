b6ad40c41aaa35506eadcf2bb79208dfa3a6e3da
Jinsujin <sylpid003@gmail.com>
2023-01-28 18:01:58 +0900

    Init: 프로젝트 생성

---
+++ UI-Archive/UI-Archive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI-Archive
+//
+//  Created by Sujin Jin on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
