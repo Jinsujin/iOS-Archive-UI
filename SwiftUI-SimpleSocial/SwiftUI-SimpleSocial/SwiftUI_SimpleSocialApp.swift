e5eb0d92eed3e99b0651a2b907ac4d7cee22bfce
Jinsujin <sylpid003@gmail.com>
2023-02-14 16:02:38 +0900

    init: Simple Social 프로젝트 생성 (#4)

---
+++ SwiftUI-SimpleSocial/SwiftUI-SimpleSocial/SwiftUI_SimpleSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SimpleSocialApp.swift
+//  SwiftUI-SimpleSocial
+//
+//  Created by Sujin Jin on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SimpleSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
