7132e9cc31c4bb4af6a055c29eba231d4239f991
Jinsujin <sylpid003@gmail.com>
2023-02-20 21:55:51 +0900

    refac: 테마 선택 셀 화면에서 ViewState 적용 (#11)
    - State(상태값) 으로 부터 화면에 반영되는 Output 을 ViewState 객체에 반영

--- Multi-form/Multi-form/ThemeCellView.swift
+++ Multi-form/Multi-form/ThemeCellView.swift
@@ -1,6 +1,7 @@
 import ComposableArchitecture
 import SwiftUI
 
+// MARK: - Theme Reducer
 struct Theme: ReducerProtocol {
     struct State: Equatable, Identifiable {
         let type: ThemeType
@@ -21,26 +22,53 @@ struct Theme: ReducerProtocol {
     }
 }
 
+
+// MARK: - ThemeCellView
 struct ThemeCellView: View {
     let store: StoreOf<Theme>
     
+    @ObservedObject var viewStore: ViewStore<ViewState, Theme.Action>
+    
+    init(store: StoreOf<Theme>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store.scope(state: ViewState.init(state:)))
+    }
+    
+    struct ViewState: Equatable {
+        let title: String
+        let accentColor: Color
+        let systemIconName: String
+        
+        init(state: Theme.State) {
+            self.title = state.type.title
+            if !state.isChecked {
+                accentColor = .gray
+                systemIconName = "checkmark.circle"
+                return
+            }
+            accentColor = .blue
+            systemIconName = "checkmark.circle.fill"
+        }
+    }
+    
     var body: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            HStack {
-                Text(viewStore.type.title)
-                    .foregroundColor(.gray)
-                Spacer()
-                Image(systemName:
-                        viewStore.isChecked ? "checkmark.circle.fill" : "checkmark.circle")
-                .foregroundColor(.gray)
-            }
-            .padding()
-            .background(Color(red: 0.94,green: 0.94,blue: 0.94))
-            .cornerRadius(10)
-            .listRowSeparator(.hidden)
-            .onTapGesture {
-                viewStore.send(.toggled)
-            }
+        HStack {
+            Text(viewStore.title)
+                .foregroundColor(viewStore.accentColor)
+            Spacer()
+            Image(systemName: viewStore.systemIconName)
+                .foregroundColor(viewStore.accentColor)
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(viewStore.accentColor, lineWidth: 1)
+        )
+        .background(Color(red: 0.94,green: 0.94,blue: 0.94))
+        .cornerRadius(8)
+        .listRowSeparator(.hidden)
+        .onTapGesture {
+            viewStore.send(.toggled)
         }
     }
 }
