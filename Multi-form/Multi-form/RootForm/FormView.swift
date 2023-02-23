162754d6fac5f76f592c15ea7309cc1b4ff364d7
Jinsujin <sylpid003@gmail.com>
2023-02-23 17:02:09 +0900

    feat: root form 에서 현재 페이지값이 유효한지에 따라 버튼컬러 바꾸기  (#11)
    - 스크롤 제스쳐를 막고 버튼으로만 이동해야 하는 작업 필요

--- Multi-form/Multi-form/RootForm/FormView.swift
+++ Multi-form/Multi-form/RootForm/FormView.swift
@@ -1,16 +1,23 @@
 import ComposableArchitecture
 import SwiftUI
 
+/// isVerify 가 true 인 경우, '다음' 버튼 활성화
+/// 1 페이지: 하나 체크한 경우
+/// 2 페이지: 텍스트필드 2개에 값이 모두 있는 경우
 struct Form: ReducerProtocol {
     struct State: Equatable {
         var themeForm = ThemeForm.State()
         var titleForm = TitleForm.State()
+        var currentPageIndex = 0
+        let maxPageIndex = 1
     }
     
     enum Action: Equatable {
         case onAppear
         case themeForm(ThemeForm.Action)
         case titleForm(TitleForm.Action)
+        case changePageIndex(Int)
+        case nextPage
     }
     
     var body: some ReducerProtocol<State, Action> {
@@ -18,6 +25,14 @@ struct Form: ReducerProtocol {
             switch action {
             case .onAppear:
                 state = .init()
+                return .none
+                
+            case let .changePageIndex(index):
+                state.currentPageIndex = index
+                return .none
+                
+            case .nextPage:
+                state.currentPageIndex = min(state.currentPageIndex+1, state.maxPageIndex)
                 return .none
                 
             default:
@@ -35,82 +50,98 @@ struct Form: ReducerProtocol {
 
 
 struct FormView: View {
-    @Environment(\.dismiss) var dismiss
-        
-    /// isVerify 가 true 인 경우, '다음' 버튼 활성화
-    /// 1 페이지: 하나 체크한 경우
-    /// 2 페이지: 텍스트필드 2개에 값이 모두 있는 경우
-    @State private var isVerify = true
-    @State private var currentPageIndex = 0
-    
-    private let titleList = [
-        "약속 테마를 선택해 주세요!",
-        "약속명과 장소를 입력해 주세요!"
-    ]
-    
     let store: StoreOf<Form>
     
+    @ObservedObject var viewStore: ViewStore<ViewState, Form.Action>
+    @Environment(\.dismiss) var dismiss
+
     init(store: StoreOf<Form>) {
-      self.store = store
+        self.store = store
+        self.viewStore = ViewStore(self.store.scope(state: ViewState.init(state:)))
     }
     
-    var body: some View {
-        WithViewStore(store) { viewStore in
-            VStack {
-                HStack {
-                    Text("\(currentPageIndex + 1)/5")
-                    Spacer()
-                    Button {
-                        dismiss()
-                     } label: {
-                         Image(systemName: "x.circle")
-                     }
+    struct ViewState: Equatable {
+        let currentPageIndex: Int
+        let pageTitle: String
+        let pageIndicatorText: String
+        var isVerify: Bool = false
+        
+        init(state: Form.State) {
+            self.currentPageIndex = state.currentPageIndex
+            if state.currentPageIndex == 0 {
+                self.pageTitle = "약속 테마를 선택해 주세요!"
+                if state.titleForm.isValidate {
+                    self.isVerify = true
                 }
-                .padding(.bottom)
-                Text(titleList[currentPageIndex])
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom)
-                // MARK: - 하위뷰
-                TabView {
-                    TitleFormView(
-                        store: self.store.scope(
-                            state: \.titleForm,
-                            action: Form.Action.titleForm
-                        )
-                    )
-                    
-                    ThemeFormView(
-                        store: self.store.scope(
-                            state: \.themeForm,
-                            action: Form.Action.themeForm
-                        )
-                    )
+            } else {
+                self.pageTitle = "약속명과 장소를 입력해 주세요!"
+                if state.themeForm.isValidate {
+                    self.isVerify = true
                 }
-                .tabViewStyle(.page(indexDisplayMode: .never))
-                // ----
-                Spacer()
-                
-                Button {
-                    print("next button")
-                    // TODO: -
-                    // 다음 버튼을 누르면 현재 페이지인덱스 += 1
-                    // 이전 버튼을 누르면 현재 페이지인덱스 -= 1
-                    currentPageIndex = 1
-                } label: {
-                    Text("다음")
-                        .padding()
-                        .border(.black)
-                        .frame(maxWidth: .infinity)
-                }
-                .foregroundColor(.white)
-                .background(viewStore.titleForm.isValidate ? .pink: .gray)
-//                .background(viewStore.themeForm.isValidate ? .pink: .gray)
-                .cornerRadius(10)
-                
             }
-            .padding()
-            .onAppear { viewStore.send(.onAppear) }
+            
+            pageIndicatorText = "\(state.currentPageIndex+1)/\(state.maxPageIndex+1)"
         }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(viewStore.pageIndicatorText)
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "x.circle")
+                }
+            }
+            .padding(.bottom)
+            Text(viewStore.pageTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom)
+            // MARK: - 하위뷰
+            TabView(selection:
+                        viewStore.binding(
+                            get: \.currentPageIndex,
+                            send: Form.Action.changePageIndex)) {
+                TitleFormView(
+                    store: self.store.scope(
+                        state: \.titleForm,
+                        action: Form.Action.titleForm
+                    )
+                ).tag(0)
+                
+                ThemeFormView(
+                    store: self.store.scope(
+                        state: \.themeForm,
+                        action: Form.Action.themeForm
+                    )
+                ).tag(1)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            // TODO: - 스크롤 제스쳐 막기(버튼으로만 다음페이지 이동)
+            //                .onAppear(perform: {
+            //                   UIScrollView.appearance().bounces = false
+            //                 })
+            
+            // ----
+            Spacer()
+            
+            Button {
+                viewStore.send(.nextPage)
+            } label: {
+                Text("다음")
+                    .padding()
+                    .border(.black)
+                    .frame(maxWidth: .infinity)
+            }
+            .foregroundColor(.white)
+            .background(viewStore.isVerify ? .pink: .gray)
+            .cornerRadius(10)
+        }
+        .padding()
+        .onAppear { viewStore.send(.onAppear) }
     }
 }
 
