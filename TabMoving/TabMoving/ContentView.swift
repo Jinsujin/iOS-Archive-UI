f23f9da7c9b0079e1b288a93b7aa806a5a55de9d
Jinsujin <sylpid003@gmail.com>
2023-02-26 18:16:08 +0900

    feat: 상단의 탭뷰로 컨텐츠 화면 이동 (#13)
    - 2개의 탭 아이템을 3개로 만들어 보고, 상수값으로 계산하는 로직을 리펙토링하기

--- TabMoving/TabMoving/ContentView.swift
+++ TabMoving/TabMoving/ContentView.swift
@@ -1,16 +1,77 @@
 import SwiftUI
 
-struct ContentView: View {
+struct ListView: View {
+    struct CellModel: Identifiable {
+        var id = UUID()
+        let title: String
+        let names: [String]
+    }
+    
+    private var models: [CellModel] = [.init(title: "타이틀1", names: ["A", "B", "C"])]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(models) { item in
+            VStack {
+                Text(item.title)
+                    .bold()
+                
+                Text(item.names.joined(separator: ", "))
+            }
+            .background(.purple)
         }
-        .padding()
     }
 }
+
+struct ContentView: View {
+    @State private var selectedIndex = 0
+    private let menus = ["메뉴1", "메뉴2"]
+    
+    var body: some View {
+        
+        NavigationView {
+            // 하위뷰(HeaderTabView)에 상위뷰(VStack)의 크기를 넣어주기 위해 사용
+            GeometryReader { geo in
+                VStack {
+                    HeaderTabView(
+                        activeIndex: $selectedIndex,
+                        menus: menus,
+                        fullWidth: geo.size.width,
+                        spacing: 60,
+                        horizontalInset: 40)
+                    
+                    // Pages
+                    TabView(selection: $selectedIndex) {
+                        ListView()
+                            .tag(0)
+                        
+                        ListView()
+                            .tag(1)
+                    }
+                    // selectedIndex 가 바뀌었을때 컨텐츠 슬라이드
+                    .animation(.default, value: selectedIndex)
+                    .background(.gray)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 200)
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                }
+                .background(.orange)
+                .navigationTitle("약속 관리")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            print("Add Item")
+                        } label: {
+                            Image(systemName: "plus")
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ TabMoving/TabMoving/HeaderTabView.swift
@@ -0,0 +1,84 @@
+import SwiftUI
+
+struct HeaderTabView: View {
+    
+    let menus: [String]
+    
+    @Binding var activeIndex: Int
+    @State private var barX: CGFloat = 0
+    
+    /// 각 버튼의 leading X position : 바 애니메이션에 필요
+    private let spacing: CGFloat
+    private let buttonLeadings: [CGFloat]
+    private let barWidth: CGFloat
+    private let buttonWidth: CGFloat
+    private let fullWidth: CGFloat
+    
+    // 초기화할때 각 버튼의 leading 값을 배열에 넣어두기
+    // -> widths[activeIndex] 로 접근 가능
+    init(
+        activeIndex: Binding<Int>,
+        menus: [String],
+        fullWidth: CGFloat,
+        spacing: CGFloat,
+        horizontalInset: CGFloat
+    ) {
+        self.menus = menus
+        self._activeIndex = activeIndex
+        
+        // |옆간격|버튼| 패딩 |버튼|옆간격|
+        // 전체크기 = (버튼*2) + 패딩 + 양옆 간격
+        self.fullWidth = fullWidth - horizontalInset
+        self.spacing = spacing
+        self.buttonWidth = (self.fullWidth-spacing)/CGFloat(menus.count)
+        self.barWidth = buttonWidth
+        self.buttonLeadings = [0, barWidth+spacing]
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: spacing) {
+                ForEach(0..<menus.count, id: \.self) { row in
+                    Button {
+                        activeIndex = row
+                        withAnimation {
+                            barX = buttonLeadings[row]
+                        }
+                    } label: {
+                        Text(menus[row])
+                            .frame(maxWidth: buttonWidth)
+                        //                            .border(.orange, width: 1)
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }
+            }
+            .frame(width: fullWidth, height: 40)
+            
+            Rectangle()
+                .frame(width: barWidth, height: 2)
+                .alignmentGuide(.leading) { $0[.leading] }
+                .offset(.init(width: barX, height: 0))
+        }
+        // 이 view 에서 특정한값(activeIndex) 이 변경되었을때, 이벤트 발생!
+        // 1. 컨텐츠를 스크롤 제스쳐로 이동했을때
+        // 2. 탭 아이템을 터치했을때
+        .onChange(of: activeIndex) { selectedRow in
+            withAnimation {
+                barX = buttonLeadings[selectedRow]
+            }
+        }
+        .background(.yellow)
+    }
+}
+
+struct HeaderTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderTabView(
+            activeIndex: .constant(0),
+            menus: ["menu-A", "munu-B"],
+            fullWidth: UIScreen.main.bounds.width,
+            spacing: 40,
+            horizontalInset: 10)
+    }
+}
