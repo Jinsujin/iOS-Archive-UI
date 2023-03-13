1a2f11f6d594f077f9731eaee59a8e33e9b1236a
Jinsujin <sylpid003@gmail.com>
2023-03-13 23:55:00 +0900

    Feat: KeyPath 를 이용해 View 제네릭하게 사용하기

--- TabMoving/AppPackage/Sources/Vanilla/ContentListView.swift
+++ TabMoving/AppPackage/Sources/Vanilla/ContentListView.swift
@@ -1,5 +1,21 @@
 import SwiftUI
 
+// MARK: - Models
+struct Todo: Identifiable {
+    let id = UUID()
+    let title: String
+    let endDate: String
+    let startDate: String
+}
+
+struct Cook: Identifiable {
+    let id = UUID()
+    let title: String
+    let emoji: String
+}
+
+
+// MARK: - ContentListView
 struct ContentListView: View {
     struct CellModel: Identifiable {
         var id = UUID()
@@ -7,16 +23,63 @@ struct ContentListView: View {
         let names: [String]
     }
     
-    private var models: [CellModel] = [.init(title: "타이틀1", names: ["A", "B", "C"])]
+    private var models: [CellModel] = [
+        .init(title: "타이틀1", names: ["A", "B", "C"])
+    ]
+    
+    private let todoList = [
+        Todo(title: "Todo1", endDate: "03-11", startDate: "03-24"),
+        Todo(title: "Todo2", endDate: "04-02", startDate: "03-09")
+    ]
+    
+    private let cookList = [
+        Cook(title: "김치찜", emoji: "🍝"),
+        Cook(title: "계란말이", emoji: "🍳"),
+        Cook(title: "카레", emoji: "🍛")
+    ]
     
     var body: some View {
-        List(models) { item in
-            VStack {
-                Text(item.title)
-                    .bold()
-                
-                Text(item.names.joined(separator: ", "))
+        List {
+            Section("할일 리스트") {
+                ForEach(todoList) { todo in
+                    DetailView(
+                        item: todo,
+                        keyPaths: .init(titleKeyPath: \.title, subtitleKeyPath: \.endDate)
+                    )
+                }
             }
+                
+            Section("요리 리스트") {
+                ForEach(cookList) { cook in
+                    DetailView(
+                        item: cook,
+                        keyPaths: .init(titleKeyPath: \.title, subtitleKeyPath: \.emoji)
+                    )
+                }
+            }
+        }
+    }
+}
+  
+// MARK: - DetailView<Model>
+struct DetailView<Model>: View {
+    struct KeyPaths {
+        let titleKeyPath: KeyPath<Model, String>
+        let subtitleKeyPath: KeyPath<Model, String>
+    }
+    
+    let title: String
+    let description: String
+    
+    init(item: Model, keyPaths: KeyPaths) {
+        self.title = item[keyPath: keyPaths.titleKeyPath]
+        self.description = item[keyPath: keyPaths.subtitleKeyPath]
+    }
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            Text(description)
         }
     }
 }
