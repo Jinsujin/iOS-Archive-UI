37c00818d3ed7fe415d5f21ef7c58605053a0858
Jinsujin <sylpid003@gmail.com>
2023-03-15 23:54:51 +0900

    feat: CounterVIew 생성

--- TabMoving/AppPackage/Sources/ComposableExample/ContentListView.swift
+++ TabMoving/AppPackage/Sources/ComposableExample/ContentListView.swift
@@ -1,4 +1,74 @@
+import ComposableArchitecture
 import SwiftUI
+
+// MARK: - Counter
+struct Counter: ReducerProtocol {
+    struct State: Equatable {
+        var count: Int
+    }
+    
+    enum Action: Equatable {
+        case dismiss
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .dismiss:
+                return .none
+            }
+        }
+    }
+}
+
+struct CounterView: View {
+    let store: StoreOf<Counter>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Text("Count")
+                Text("\(viewStore.count)")
+            }
+        }
+    }
+}
+
+
+// MARK: - ListFeature
+struct ListFeature: ReducerProtocol {
+    struct State: Equatable {
+        var optionalCounter: Counter.State?
+    }
+    
+    enum Action: Equatable {
+        case optionalCounter(Counter.Action)
+        case onAppear
+        case showCounter
+    }
+
+    
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .showCounter:
+                state.optionalCounter = .init(count: 1)
+                return .none
+                
+            case .onAppear:
+                state.optionalCounter = .init(count: 10)
+                return .none
+                
+            case .optionalCounter:
+                return .none
+            }
+        }
+        .ifLet(\.optionalCounter, action: /Action.optionalCounter) {
+            Counter()
+        }
+    }
+}
 
 struct ContentListView: View {
     struct CellModel: Identifiable {
@@ -9,13 +79,45 @@ struct ContentListView: View {
     
     private var models: [CellModel] = [.init(title: "타이틀1", names: ["A", "B", "C"])]
     
+    private let store: StoreOf<ListFeature>
+    
+    init() {
+        self.store = .init(
+            initialState: ListFeature.State(),
+            reducer: ListFeature()._printChanges())
+    }
+    
     var body: some View {
-        List(models) { item in
-            VStack {
-                Text(item.title)
-                    .bold()
-                
-                Text(item.names.joined(separator: ", "))
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            NavigationStack {
+                VStack {
+                    IfLetStore(
+                        self.store.scope(
+                            state: \.optionalCounter,
+                            action: ListFeature.Action.optionalCounter
+                        )
+                    ) {
+                        CounterView(store: $0)
+                    }
+                    
+                    List(models) { item in
+                        VStack {
+                            Text(item.title)
+                                .bold()
+                            
+                            Text(item.names.joined(separator: ", "))
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            viewStore.send(.showCounter)
+                        } label: {
+                            Text("카운트 시작")
+                        }
+                    }
+                }
             }
         }
     }
